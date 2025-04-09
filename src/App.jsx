76d8b649e474c0247e6be4317f87c3815fd15390
Pedro Silva <pedronestjp@gmail.com>
2025-04-09 19:57:21 -0300

    criei uma pagina de teste

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import DogGallery from "./pages/DogGallery";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import Teste from "./pages/Teste";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dogs" element={<DogGallery />} />
         <Route path="/about" element={<About />} />
+        <Route path="/teste" element={<Teste />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
     </div>
   );
